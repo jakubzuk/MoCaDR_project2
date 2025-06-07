f0a4d5d10386c9c3835421f88519a11a35455325
AdxSa <AdSam250250@gmail.com>
2025-06-07 21:44:13 +0200

    added data generation functionality

--- motif_123456_generate.py
+++ motif_123456_generate.py
@@ -36,10 +36,27 @@ ThetaB =np.asarray(params['ThetaB'])
 # according to the description from the script, and save them in a .csv file
 # the i-th row = x_i
 
+def generate_sample(w, k, alpha, Theta, ThetaB):
+    Z = np.random.choice([0, 1], size=k, p=[1 -alpha, alpha])
+    X = np.zeros(shape=(k, w))
+
+    for i in range(k):
+        if Z[i]:
+            r = np.zeros(shape=w)
+            for j in range(w):
+                r[j] = np.random.choice(np.arange(1, 5, step=1), p=Theta[:, j])
+            X[i] = r
+        else:
+            X[i] = np.random.choice(np.arange(1, 5, step=1), size=w, p=ThetaB)
+    return X, alpha
+
+
+X, alpha = generate_sample(w, k, alpha, Theta, ThetaB)
+
 # For example, assume that x_1, ..., x_k are collected in a matrix X
 # (as a reminder: each x_{ij} is A, C, G, or T, which we identify with 1, 2, 3, 4)
 
-X = np.random.randint(4, size=(k, w)) + 1
+# X = np.random.randint(4, size=(k, w)) + 1
 
 # We need to save the matrix X and the value of alpha (k and w can be inferred from X)
 
@@ -54,3 +71,5 @@ gen_data = {
 with open(output_file, 'w') as outfile:
     json.dump(gen_data, outfile)
  
+
+
