43121735b856e796ef6f1bbf8ae1f2126994b81a
AdxSa <AdSam250250@gmail.com>
2025-06-08 15:23:47 +0200

    repaired EM algorithm implementation

--- bez_syfu.py
+++ bez_syfu.py
@@ -1,14 +1,28 @@
 import numpy as np
+import json
 
+with open("generated_data.json", 'r') as inputfile:
+    params = json.load(inputfile)
+
+alpha = params['alpha']
+X = params['X']
 w = 3
-alpha = 0.5
 k = 10
-Theta = np.array([[0.375, 0.1, 0.14285714285714285], [0.125, 0.2, 0.2857142857142857],
-          [0.25, 0.3, 0.14285714285714285], [0.25, 0.4, 0.42857142857142855]])
-ThetaB = np.array([0.25, 0.25, 0.25, 0.25])
-X = np.random.randint(0, 4, (k, w))
-print(X)
-print(Theta[X, np.arange(w)])
+# Theta = np.array([[0.375, 0.1, 0.14285714285714285], [0.125, 0.2, 0.2857142857142857],
+#           [0.25, 0.3, 0.14285714285714285], [0.25, 0.4, 0.42857142857142855]])
+# ThetaB = np.array([0.25, 0.25, 0.25, 0.25])
+# X = np.random.randint(0, 4, (k, w))
+# print(X)
+# print(Theta[X, np.arange(w)])
+
+Theta = np.random.uniform(0, 1, 4*w)
+Theta = Theta.reshape(4, w)
+Theta = Theta / Theta.sum(axis=0)
+print(Theta)
+
+ThetaB = np.random.uniform(0,1, 4)
+ThetaB = ThetaB / ThetaB.sum()
+print(ThetaB)
 
 def compute_log_likehood(X, alpha, Theta, ThetaB):
     k, w = X.shape
@@ -17,46 +31,62 @@ def compute_log_likehood(X, alpha, Theta, ThetaB):
 
     return np.sum(np.log(alpha * np.exp(log_motif) + (1 - alpha) * np.exp(log_bg)))
 
-def em_algorithm(X, alpha, Theta, ThetaB, max_iter = 1000, tol=1e-5):
+def em_algorithm(X, alpha, Theta, ThetaB, max_iter = 1000, tol=1e-10):
+
     X = np.array(X, dtype=int)
+    k, w = X.shape
     Theta = np.array(Theta)
     ThetaB = np.array(ThetaB)
+    maximal_t = 0
     for t in range(max_iter):
 
         # Expectation
 
-        log_motif = np.sum(np.log(Theta[X, np.arange(w)]), axis=1)
-        log_bg = np.sum(np.log(ThetaB[X]), axis=1)
-        
-        p_motif = np.exp(log_motif)
-        p_bg = np.exp(log_bg)
+        # log_motif = np.sum(np.log(Theta[X, np.arange(w)]), axis=1)
+        # log_bg = np.sum(np.log(ThetaB[X]), axis=1)
 
-        gamma = (alpha * p_motif) / (alpha * p_motif + (1 - alpha) * p_bg)
+        # ll = np.sum(np.log(alpha * np.exp(log_motif) + (1 - alpha) * np.exp(log_bg)))
 
-        ll = compute_log_likehood(X, alpha, Theta, ThetaB)
+        # p_motif = np.exp(log_motif)
+        # p_bg = np.exp(log_bg)
+
+        p_motif = np.prod(Theta[X, np.arange(w)], axis=1)
+        p_bg = np.prod(ThetaB[X], axis=1)
+
+        ll = np.sum(np.log(alpha * p_motif + (1 - alpha) * p_bg))
+
+        Q1 = (alpha * p_motif) / sum([alpha * p_motif, (1-alpha) * p_bg])
+
+        # ll = compute_log_likehood(X, alpha, Theta, ThetaB)
 
         # Maximization
 
         for j in range(w):
             counts_motif = np.zeros(4)
             for i in range(k):
-                counts_motif[X[i, j]] += gamma[i]
-            Theta[:, j] = counts_motif / gamma.sum()
+                counts_motif[X[i, j]] += Q1[i]
+            Theta[:, j] = counts_motif / Q1.sum()
 
         counts_bg = np.zeros(4)
         for i in range(k):
-            counts_bg += (1 - gamma[i]) * np.bincount(X[i], minlength=4)
+            counts_bg += (1 - Q1[i]) * np.bincount(X[i], minlength=4)
         ThetaB = counts_bg / counts_bg.sum()
 
-        new_ll = compute_log_likehood(X, alpha, Theta, ThetaB)
+        # new_ll = compute_log_likehood(X, alpha, Theta, ThetaB)
+
+        new_p_motif = np.prod(Theta[X, np.arange(w)], axis=1)
+        new_p_bg = np.prod(ThetaB[X], axis=1)
+        new_ll = np.sum(np.log(alpha * new_p_motif + (1 - alpha) * new_p_bg))
+
+        maximal_t += 1
 
         if abs(new_ll - ll) < tol:
+            print(f"number of iterations: {maximal_t}")
             break
+    Theta, ThetaB = Theta.round(3), ThetaB.round(3)
+    return Theta, ThetaB
 
-    return Theta, ThetaB 
-    
 theta, thetaB = em_algorithm(X, alpha, Theta, ThetaB)
 
-print(compute_log_likehood(X, alpha, Theta, ThetaB))
-print(theta)
-print(thetaB)
+# print(theta)
+# print(thetaB)
--- helper_functions.py
+++ helper_functions.py
@@ -7,40 +7,57 @@ def compute_log_likehood(X, alpha, Theta, ThetaB):
 
     return np.sum(np.log(alpha * np.exp(log_motif) + (1 - alpha) * np.exp(log_bg)))
 
-def em_algorithm(X, alpha, Theta, ThetaB, max_iter = 1000, tol=1e-5):
+def em_algorithm(X, alpha, Theta, ThetaB, max_iter = 1000, tol=1e-10):
+
     X = np.array(X, dtype=int)
+    k, w = X.shape
     Theta = np.array(Theta)
     ThetaB = np.array(ThetaB)
+    maximal_t = 0
     for t in range(max_iter):
 
         # Expectation
 
-        log_motif = np.sum(np.log(Theta[X, np.arange(w)]), axis=1)
-        log_bg = np.sum(np.log(ThetaB[X]), axis=1)
-        
-        p_motif = np.exp(log_motif)
-        p_bg = np.exp(log_bg)
+        # log_motif = np.sum(np.log(Theta[X, np.arange(w)]), axis=1)
+        # log_bg = np.sum(np.log(ThetaB[X]), axis=1)
 
-        gamma = (alpha * p_motif) / (alpha * p_motif + (1 - alpha) * p_bg)
+        # ll = np.sum(np.log(alpha * np.exp(log_motif) + (1 - alpha) * np.exp(log_bg)))
 
-        ll = compute_log_likehood(X, alpha, Theta, ThetaB)
+        # p_motif = np.exp(log_motif)
+        # p_bg = np.exp(log_bg)
+
+        p_motif = np.prod(Theta[X, np.arange(w)], axis=1)
+        p_bg = np.prod(ThetaB[X], axis=1)
+
+        ll = np.sum(np.log(alpha * p_motif + (1 - alpha) * p_bg))
+
+        Q1 = (alpha * p_motif) / sum([alpha * p_motif, (1-alpha) * p_bg])
+
+        # ll = compute_log_likehood(X, alpha, Theta, ThetaB)
 
         # Maximization
 
         for j in range(w):
             counts_motif = np.zeros(4)
             for i in range(k):
-                counts_motif[X[i, j]] += gamma[i]
-            Theta[:, j] = counts_motif / gamma.sum()
+                counts_motif[X[i, j]] += Q1[i]
+            Theta[:, j] = counts_motif / Q1.sum()
 
         counts_bg = np.zeros(4)
         for i in range(k):
-            counts_bg += (1 - gamma[i]) * np.bincount(X[i], minlength=4)
+            counts_bg += (1 - Q1[i]) * np.bincount(X[i], minlength=4)
         ThetaB = counts_bg / counts_bg.sum()
 
-        new_ll = compute_log_likehood(X, alpha, Theta, ThetaB)
+        # new_ll = compute_log_likehood(X, alpha, Theta, ThetaB)
+
+        new_p_motif = np.prod(Theta[X, np.arange(w)], axis=1)
+        new_p_bg = np.prod(ThetaB[X], axis=1)
+        new_ll = np.sum(np.log(alpha * new_p_motif + (1 - alpha) * new_p_bg))
+
+        maximal_t += 1
 
         if abs(new_ll - ll) < tol:
+            print(f"number of iterations: {maximal_t}")
             break
-
-    return Theta, ThetaB 
+    Theta, ThetaB = Theta.round(3), ThetaB.round(3)
+    return Theta, ThetaB
